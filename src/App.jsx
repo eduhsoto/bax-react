f1306e61fb0bb7623b00e2b44793e39c8bee9542
Eduardo Hernández Soto <hseduardo@gmail.com>
2022-12-15 19:03:46 -0600

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import './assets/index.css'
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
