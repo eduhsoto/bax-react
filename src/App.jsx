336a72b155adc66ac53e3c8ee8d95d38e249682f
Eduardo Hernández Soto <hseduardo@gmail.com>
2022-12-19 17:02:14 -0600

    page contact{

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import Navbar from "./components/NavBar";
 import CourseP from "./pages/CourseP";
 import Home from "./pages/Home";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/courses" element={<CourseP />} />
+        <Route path="/contact" element={<Contact />} />
+
       </Routes>
       <Footer />
     </BrowserRouter>
