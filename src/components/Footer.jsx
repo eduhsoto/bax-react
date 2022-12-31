8bff495f75a1843125f44ff06b06cc40ba71ae2b
Eduardo Hernández Soto <hseduardo@gmail.com>
2022-12-31 14:02:39 -0600

    footer styles from tailwind

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,71 +1,80 @@
+import { NavLink } from "react-router-dom";
+import NavHash from "./navbar/NavHash";
+
 const Footer = () => {
   return (
-    <footer className="footer__color">
-      <div className="container card___grid grid__footer">
-        <div className="flex__column flex__course-column">
-          <a href="/courses" className="item__footer p__small__roboto">
+    <footer className="bg-color_tertiary mt-36 py-8">
+      <div className="mx-16 grid grid-cols-3-auto gap-16 justify-items-center items-center pb-5 border-b-2 border-color_secondary_2 tablet:mx-8 phone:mx-4">
+        <div className="flex flex-col gap-4 phone:items-center">
+          <NavLink
+            to="/courses"
+            className="font-roboto text-base leading-[19px] font-normal text-color_default"
+          >
             Cursos
-          </a>
-          <a href="/#testimonials" className="item__footer p__small__roboto">
-            Testimonios
-          </a>
-          <a href="/#about" className="item__footer p__small__roboto">
-            Nosotros
-          </a>
-          <a href="/contact" className="item__footer p__small__roboto">
+          </NavLink>
+          <NavHash
+            hashNav="#testimonials"
+            hashName="Testimonios"
+            classHash={false}
+          />
+          <NavHash hashNav="#about" hashName="Nosotros" classHash={false} />
+          <NavLink
+            to="/contact"
+            className="font-roboto text-base leading-[19px] font-normal text-color_default"
+          >
             Contacto
-          </a>
+          </NavLink>
         </div>
 
         <img
           src="./src/assets/img/brand-logo-footer.png"
           alt="brand logo footer"
-          className="brand__logo__footer"
+          className="w-[40%]"
         />
 
-        <div className="social__footer">
+        <div className="flex gap-4">
           <a href="">
             <img
               src="./src/assets/img/facebook-icon.png"
               alt="facebook icon"
-              className="social__icon"
+              className="w-10"
             />
           </a>
           <a href="">
             <img
               src="./src/assets/img/instagram-icon.png"
               alt="instagram icon"
-              className="social__icon"
+              className="w-10"
             />
           </a>
           <a href="">
             <img
               src="../src/assets/img/linkedin-icon.png"
               alt="linkedin icon"
-              className="social__icon"
+              className="w-10"
             />
           </a>
           <a href="">
             <img
               src="./src/assets/img/email-icon.png"
               alt="email icon"
-              className="social__icon"
+              className="w-10"
             />
           </a>
           <a href="">
             <img
               src="./src/assets/img/twitter-icon.png"
               alt="twitter icon"
-              className="social__icon"
+              className="w-10"
             />
           </a>
         </div>
       </div>
-      <p className="p__copyright p__small__roboto">
+      <p className="mx-[60px] my-5 font-roboto text-base font-normal leading-[19px] text-color_default">
         Copyright Inc. Bax. © 2022. Design by{" "}
         <a
           href="https://www.linkedin.com/in/debora-migliani/"
-          className="p__small__roboto"
+          className="font-roboto text-base font-normal leading-[19px] text-color_default"
         >
           Debora Migliani Diseñadora UX/UI
         </a>
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -26,14 +26,20 @@ const NavBar = () => {
 
         <div
           className={`tablet:fixed tablet:top-20 tablet:w-2/4 tablet:left-1/3 tablet:bg-color_default tablet:py-5 tablet:px-5 tablet:shadow-bax-shadow tablet:rounded-bax-radius duration-500 ${
-            clicked ? "opacity-100 visible" : "tablet:opacity-0 tablet:invisible"
+            clicked
+              ? "opacity-100 visible"
+              : "tablet:opacity-0 tablet:invisible"
           }`}
         >
           <div className="tablet:flex tablet:flex-col tablet:justify-between tablet:items-center tablet:gap-8">
             <NavLi itemNav="Inicio" linkTo="/" />
             <NavLi itemNav="Cursos" linkTo="/courses" />
-            <NavHash hashNav="#testimonials" hashName="Testimonios" />
-            <NavHash hashNav="#about" hashName="Nosotros" />
+            <NavHash
+              hashNav="#testimonials"
+              hashName="Testimonios"
+              classHash={true}
+            />
+            <NavHash hashNav="#about" hashName="Nosotros" classHash={true} />
             <NavLi itemNav="Contacto" linkTo="/contact" />
           </div>
         </div>
