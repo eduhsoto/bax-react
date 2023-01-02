861cb999126bdc77e0848098d64a9f2933d0aa00
Eduardo Hernández Soto <hseduardo@gmail.com>
2023-01-01 19:41:38 -0600

    hidden component according page not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import "./assets/index.css";
-import Footer from "./components/Footer";
-import Navbar from "./components/navbar/NavBar";
 import CourseP from "./pages/CourseP";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
+import { NavbarwithRenderB, FooterwithRenderB } from "./LocationHco";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
+      <NavbarwithRenderB />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/courses" element={<CourseP />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/page-not-found" element={<NotFound />} />
+        <Route path="/*" element={<Navigate to="/page-not-found" />} />
       </Routes>
-      <Footer />
+      <FooterwithRenderB />
     </BrowserRouter>
   );
 }
---
+++ src/LocationHco.jsx
@@ -0,0 +1,14 @@
+import { useLocation } from "react-router-dom";
+import NavBar from "./components/navbar/NavBar";
+import Footer from "./components/Footer";
+
+const withRenderBoolean = (Component) => {
+  const WrappedComponent = () => {
+    const { pathname } = useLocation();
+    return <>{pathname !== "/page-not-found" && <Component />}</>;
+  };
+  return WrappedComponent;
+};
+
+export const NavbarwithRenderB = withRenderBoolean(NavBar);
+export const FooterwithRenderB = withRenderBoolean(Footer);
